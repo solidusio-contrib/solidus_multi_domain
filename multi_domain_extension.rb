9313fdc7a50e6ac5213c52a51d655c1f46e6c055
David North <davidnorth@railsdog.com>
2010-06-16 14:16:46 -0500

    Fix that if Admin::ProductsController doesnt already have a hook defined for create.before, as is the case in latest spree, an exception was raised when trying to add :set_stores with << because create.before returned nil, not an empty array

--- multi_domain_extension.rb
+++ multi_domain_extension.rb
@@ -127,3 +127,26 @@ class MultiDomainExtension < Spree::Extension
     # end
   end
 end
+
+
+# Make it possible to add to existing resource controller hooks with '<<' even when there's no hook of a given type defined yet.
+# e.g. create.before << :assign_to_store
+ResourceController::Accessors.module_eval do
+  private
+  def block_accessor(*accessors)
+    accessors.each do |block_accessor|
+      class_eval <<-"end_eval", __FILE__, __LINE__
+
+        def #{block_accessor}(*args, &block)
+          @#{block_accessor} ||= []
+          unless args.empty? && block.nil?
+            args.push block if block_given?
+            @#{block_accessor} = [args].flatten
+          end
+          @#{block_accessor}
+        end
+
+      end_eval
+    end
+  end
+end
