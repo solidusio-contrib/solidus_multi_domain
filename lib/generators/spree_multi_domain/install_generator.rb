08120073a2816b59a387ff47bafaff62a0497922
Roman Smirnov <roman@railsdog.com>
2010-09-16 23:33:19 +0400

    Added install generator

---
+++ lib/generators/spree_multi_domain/install_generator.rb
@@ -0,0 +1,15 @@
+module SpreeMultiDomain
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../templates", __FILE__)
+
+      desc "Configures your Rails application for use with spree_multi_domain"
+
+      def copy_migrations
+        directory "db"
+      end
+
+    end
+  end
+end
+
