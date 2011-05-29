68bddcf71ebbf61bda0da0c32e06c8e40767778b
Roman Smirnov <roman@railsdog.com>
2011-05-29 20:52:21 +0400

    Switch to Deface

--- lib/spree_multi_domain_hooks.rb
+++ lib/spree_multi_domain_hooks.rb
@@ -1,19 +1,55 @@
-class SpreeMultiDomainHooks < Spree::ThemeSupport::HookListener
+Deface::Override.new(
+  :virtual_path => "admin/products/index",
+  :name => "multi_domain_admin_products_index_headers",
+  :insert_before => "[data-hook='admin_products_index_header_actions']",
+  :partial => "admin/products/index_headers",
+  :disabled => false)
 
-  insert_after :admin_products_index_headers, :partial => "admin/products/index_headers"
-  insert_after :admin_products_index_rows, :partial => "admin/products/index_rows"
-  insert_before :admin_products_index_search, :partial => "admin/products/index_search_fields"
+Deface::Override.new(
+  :virtual_path => "admin/products/index",
+  :name => "multi_domain_admin_products_index_rows",
+  :insert_before => "[data-hook='admin_products_index_row_actions']",
+  :partial => "admin/products/index_rows",
+  :disabled => false)
+  
+Deface::Override.new(
+  :virtual_path => "admin/products/index",
+  :name => "multi_domain_admin_products_index_search",
+  :insert_top => "[data-hook='admin_products_index_search']",
+  :partial => "admin/products/index_search_fields",
+  :disabled => false)  
 
-  insert_after :admin_product_form_meta, :partial => "admin/products/stores"
+Deface::Override.new(
+  :virtual_path => "admin/products/_form",
+  :name => "multi_domain_admin_product_form_meta",
+  :insert_bottom => "[data-hook='admin_product_form_meta']",
+  :partial => "admin/products/stores",
+  :disabled => false)
 
-  insert_after :admin_trackers_index_headers do
-    "<th>#{I18n.t(:store)}</th>"
-  end
-  insert_after :admin_trackers_index_rows, 'admin/trackers/index_rows'
-  replace :additional_tracker_fields, 'admin/trackers/store'
+Deface::Override.new(
+  :virtual_path => "admin/trackers/index",
+  :name => "multi_domain_admin_trackers_index_headers",
+  :insert_before => "[data-hook='admin_trackers_index_headers'] th:last",
+  :text => "<th><%= I18n.t(:store) %></th>",
+  :disabled => false)
 
-  insert_after :admin_configurations_menu do
-    "<%= configurations_menu_item(I18n.t('stores_admin'), admin_stores_url, I18n.t('manage_stores')) %>"
-  end
+Deface::Override.new(
+  :virtual_path => "admin/trackers/index",
+  :name => "multi_domain_admin_trackers_index_rows",
+  :insert_before => "[data-hook='admin_trackers_index_rows'] td:last",
+  :partial => "admin/trackers/index_rows",
+  :disabled => false)
 
-end
+Deface::Override.new(
+  :virtual_path => "admin/trackers/_form",
+  :name => "multi_domain_additional_tracker_fields",
+  :replace => "[data-hook='additional_tracker_fields']",
+  :partial => "admin/trackers/store",
+  :disabled => false)
+
+Deface::Override.new(
+  :virtual_path => "admin/configurations/index",
+  :name => "multi_domain_admin_configurations_menu",
+  :insert_bottom => "[data-hook='admin_configurations_menu']",
+  :text => "<%= configurations_menu_item(I18n.t('stores_admin'), admin_stores_url, I18n.t('manage_stores')) %>",
+  :disabled => false)
