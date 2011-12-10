cc706a51ddf649018797e468fbc9763de665ce21
Ryan Bigg <radarlistener@gmail.com>
2011-12-10 16:48:02 +1100

    find_layout method has been moved to ActionView::TemplateRendered

--- lib/spree_multi_domain.rb
+++ lib/spree_multi_domain.rb
@@ -94,20 +94,21 @@ module SpreeBase
 end
 
 
-module ActionView::Layouts
+module ActionView
+  class TemplateRenderer
+    def find_layout_with_multi_store(layout)
+      store_layout = layout
+      if respond_to?(:current_store) && current_store && !controller.is_a?(Admin::BaseController)
+        store_layout = layout.gsub("layouts/", "layouts/#{current_store.code}/")
+      end
+      begin
+        find_layout_without_multi_store(store_layout)
+      rescue ::ActionView::MissingTemplate
+        find_layout_without_multi_store(layout)
+      end
+    end
 
-  def find_layout_with_multi_store(layout)
-    store_layout = layout
-    if respond_to?(:current_store) && current_store && !controller.is_a?(Admin::BaseController)
-      store_layout = layout.gsub("layouts/", "layouts/#{current_store.code}/")
-    end
-    begin
-      find_layout_without_multi_store(store_layout)
-    rescue ::ActionView::MissingTemplate
-      find_layout_without_multi_store(layout)
-    end
+    alias_method_chain :find_layout, :multi_store
   end
-
-  alias_method_chain :find_layout, :multi_store
 
 end
