jumph4x/spree-multi-domain

Spree 1-0 initial

app/controllers/spree/admin/stores_controller.rb
@@ -1,4 +1,4 @@
-class Admin::StoresController < Admin::BaseController
+class Spree::Admin::StoresController < Spree::Admin::BaseController
   resource_controller
   ssl_required
 

solidusio-contrib/solidus_multi_domain

app/controllers/spree/admin/stores_controller.rb
@@ -1,0 +1,3 @@
+class Spree::Admin::StoresController < Spree::Admin::ResourceController
+
+end