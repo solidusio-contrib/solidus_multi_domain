0ce67019f20952d6d66d3949e37cf0adf50924ca
Roman Smirnov <roman@railsdog.com>
2012-05-28 15:10:11 +0400

    Added missing join_table option to Spree::Store#products association

--- app/models/spree/store.rb
+++ app/models/spree/store.rb
@@ -1,6 +1,6 @@
 module Spree
   class Store < ActiveRecord::Base
-    has_and_belongs_to_many :products
+    has_and_belongs_to_many :products, :join_table => 'spree_products_stores'
     has_many :taxonomies
     has_many :orders
 
