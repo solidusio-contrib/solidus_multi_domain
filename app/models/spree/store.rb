cd69ce363403dba2323e0c4410ad4196a03d31b0
johndavid400 <jd@isotope11.com>
2012-08-14 14:02:42 -0500

    updated Store model to include attr_accessible for form attributes. will now allow you to create new store with Rails 3.2

--- app/models/spree/store.rb
+++ app/models/spree/store.rb
@@ -5,15 +5,16 @@ module Spree
     has_many :orders
 
     validates_presence_of :name, :code, :domains
+    attr_accessible :name, :code, :default, :email, :domains
 
     scope :default, where(:default => true)
     scope :by_domain, lambda { |domain| where("domains like ?", "%#{domain}%") }
-    
+
     def self.current(domain = nil)
       current_store = domain ? Store.by_domain(domain).first : nil
       current_store || first_found_default
     end
-    
+
     def self.first_found_default
       @cached_default ||= Store.default.first
     end
