7e8d0f4339f45bef4c7325570630722b121c2603
Jonathan Roes <jon@skookum.com>
2012-10-29 15:19:34 -0400

    Make sure the stores & domains link is in the config sidebar.

--- app/overrides/decorate_admin_configurations_index.rb
+++ app/overrides/decorate_admin_configurations_index.rb
@@ -1,6 +1,5 @@
-Deface::Override.new(
-  :virtual_path => "spree/admin/configurations/index",
-  :name => "multi_domain_admin_configurations_menu",
-  :insert_bottom => "[data-hook='admin_configurations_menu']",
-  :text => "<%= configurations_menu_item(I18n.t('stores_admin'), admin_stores_url, I18n.t('manage_stores')) %>",
-  :disabled => false)
+Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
+                     :name => "multi_domain_admin_configurations_menu",
+                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
+                     :text => "<%= configurations_sidebar_menu_item t(:stores_admin), admin_stores_url %>",
+                     :disabled => false)
