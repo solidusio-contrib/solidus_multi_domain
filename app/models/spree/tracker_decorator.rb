0a2ca0f0133ca7d5e6f58fb73784d653e58ab6e6
Adam Jahn <ajjahn@gmail.com>
2012-11-29 23:02:54 -0500

    add attr_accessible store_id to tracker
    Fixes #31

--- app/models/spree/tracker_decorator.rb
+++ app/models/spree/tracker_decorator.rb
@@ -1,5 +1,6 @@
 Spree::Tracker.class_eval do
   belongs_to :store
+  attr_accessible :store_id
 
   def self.current(domain)
     Spree::Tracker.where(:active => true, :environment => Rails.env).joins(:store).where("spree_stores.domains LIKE '%#{domain}%'").first
