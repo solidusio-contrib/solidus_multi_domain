b7561df63f9b5ce38b109696ecb5afcbbbf6a543
Gregor MacDougall <gregor@freerunningtechnologies.com>
2012-12-11 15:47:51 -0800

    Admin interface for setting payment methods.
    If multiple payment methods are available, this will allow a user to
    specify which payment methods are available on certain stores.
    
    Fixes #47

--- app/controllers/spree/admin/stores_controller.rb
+++ app/controllers/spree/admin/stores_controller.rb
@@ -1,3 +1,9 @@
 class Spree::Admin::StoresController < Spree::Admin::ResourceController
 
+  before_filter :load_payment_methods
+
+  private
+    def load_payment_methods
+      @payment_methods = Spree::PaymentMethod.all
+    end
 end
--- app/models/spree/store.rb
+++ app/models/spree/store.rb
@@ -7,7 +7,7 @@ module Spree
     has_many :payment_methods, :through => :store_payment_methods
 
     validates_presence_of :name, :code, :domains
-    attr_accessible :name, :code, :default, :email, :domains, :logo, :default_currency
+    attr_accessible :name, :code, :default, :email, :domains, :logo, :default_currency,  :payment_method_ids
 
     scope :default, where(:default => true)
     scope :by_domain, lambda { |domain| where("domains like ?", "%#{domain}%") }
