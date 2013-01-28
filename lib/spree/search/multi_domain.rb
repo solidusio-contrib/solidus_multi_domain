6d1a955042ad80dfd944d08da4b7a061350154d7
Alexey Chernikov <alexey.chernikov@gmail.com>
2013-01-28 18:47:33 +0600

    Fix filter search
    Fixes #38

--- lib/spree/search/multi_domain.rb
+++ lib/spree/search/multi_domain.rb
@@ -9,6 +9,7 @@ module Spree::Search
       base_scope = get_products_conditions_for(base_scope, keywords) unless keywords.blank?
 
       base_scope = base_scope.on_hand unless Spree::Config[:show_zero_stock_products]
+      base_scope = add_search_scopes(base_scope)
       base_scope
     end
     
