b6441bb6f9cd8e5fe84df045138b2e7eb0c8026c
Ryan Bigg <radarlistener@gmail.com>
2013-04-26 13:49:50 +1000

    Allow shipped_email to take either a shipment id or a shipment object
    Fixes #54

--- app/mailers/spree/shipment_mailer.rb
+++ app/mailers/spree/shipment_mailer.rb
@@ -2,7 +2,7 @@ class Spree::ShipmentMailer < ActionMailer::Base
   helper "spree/base"
 
   def shipped_email(shipment, resend=false)
-    @shipment = shipment
+    @shipment = shipment.respond_to?(:id) ? shipment : Spree::Shipment.find(shipment)
     subject = (resend ? "[RESEND] " : "")
     subject += "#{Spree::Config[:site_name]} Shipment Notification ##{shipment.order.number}"
     mail_params = {:to => shipment.order.email, :subject => subject}
