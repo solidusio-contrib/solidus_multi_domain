217439a76162d4031560906c724591c3aa8cbef9
Daniel Dixon <code@danieldixon.com>
2013-04-29 11:09:36 -0500

    Fix to order and shipment mailers.
    Fixes #55

--- app/mailers/spree/order_mailer.rb
+++ app/mailers/spree/order_mailer.rb
@@ -2,20 +2,20 @@ class Spree::OrderMailer < ActionMailer::Base
   helper "spree/base"
 
   def confirm_email(order, resend=false)
-    find_order
+    find_order(order)
     subject = (resend ? "[RESEND] " : "")
-    subject += "#{Spree::Config[:site_name]} Order Confirmation ##{order.number}"
-    mail_params = {:to => order.email, :subject => subject}
-    mail_params[:from] = order.store.email if order.store.email.present?
+    subject += "#{Spree::Config[:site_name]} Order Confirmation ##{@order.number}"
+    mail_params = {:to => @order.email, :subject => subject}
+    mail_params[:from] = @order.store.email if @order.store.email.present?
     mail(mail_params)
   end
 
   def cancel_email(order, resend=false)
-    find_order
+    find_order(order)
     subject = (resend ? "[RESEND] " : "")
-    subject += "#{Spree::Config[:site_name]} Cancellation of Order ##{order.number}"
-    mail_params = {:to => order.email, :subject => subject}
-    mail_params[:from] = order.store.email if order.store.email.present?
+    subject += "#{Spree::Config[:site_name]} Cancellation of Order ##{@order.number}"
+    mail_params = {:to => @order.email, :subject => subject}
+    mail_params[:from] = @order.store.email if @order.store.email.present?
     mail(mail_params)
   end
 
--- app/mailers/spree/shipment_mailer.rb
+++ app/mailers/spree/shipment_mailer.rb
@@ -4,10 +4,10 @@ class Spree::ShipmentMailer < ActionMailer::Base
   def shipped_email(shipment, resend=false)
     @shipment = shipment.respond_to?(:id) ? shipment : Spree::Shipment.find(shipment)
     subject = (resend ? "[RESEND] " : "")
-    subject += "#{Spree::Config[:site_name]} Shipment Notification ##{shipment.order.number}"
-    mail_params = {:to => shipment.order.email, :subject => subject}
-    if shipment.order.store && shipment.order.store.email.present?
-      mail_params[:from] = shipment.order.store.email
+    subject += "#{Spree::Config[:site_name]} Shipment Notification ##{@shipment.order.number}"
+    mail_params = {:to => @shipment.order.email, :subject => subject}
+    if @shipment.order.store && @shipment.order.store.email.present?
+      mail_params[:from] = @shipment.order.store.email
     end
     mail(mail_params)
   end
