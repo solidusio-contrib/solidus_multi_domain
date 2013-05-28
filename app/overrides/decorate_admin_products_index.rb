aea72861991b224d30c826b32adcf6667a0a0f3f
Nelson Kelem <nelson@21waves.com>
2013-05-28 20:35:52 +0100

    Add Spree.t() and missing translations in the en.yml file
    I also changed all Spree.t("a_string_word") to Spree.t(:a_string_word)
    except for nested ones eg Spring.t("a_nested.string_word")

--- app/overrides/decorate_admin_products_index.rb
+++ app/overrides/decorate_admin_products_index.rb
@@ -4,14 +4,14 @@ Deface::Override.new(
   :insert_before => "[data-hook='admin_products_index_header_actions']",
   :partial => "spree/admin/products/index_headers",
   :disabled => false)
-  
+
 Deface::Override.new(
   :virtual_path => "spree/admin/products/index",
   :name => "multi_domain_admin_products_index_rows",
   :insert_before => "[data-hook='admin_products_index_row_actions']",
   :partial => "spree/admin/products/index_rows",
   :disabled => false)
-  
+
 Deface::Override.new(
   :virtual_path => "spree/admin/products/index",
   :name => "multi_domain_admin_products_index_search",
