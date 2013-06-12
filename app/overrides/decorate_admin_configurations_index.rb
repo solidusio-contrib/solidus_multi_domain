f577a40d248979428f3c479be7c7560077d17ce2
Daniel Dixon <code@danieldixon.com>
2013-06-12 09:46:18 -0500

    Fix translation for stores_admin
    Fixes #65

--- app/overrides/decorate_admin_configurations_index.rb
+++ app/overrides/decorate_admin_configurations_index.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
                      :name => "multi_domain_admin_configurations_menu",
                      :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-                     :text => "<%= configurations_sidebar_menu_item t(:stores_admin), admin_stores_url %>",
+                     :text => "<%= configurations_sidebar_menu_item Spree.t(:stores_admin), admin_stores_url %>",
                      :disabled => false)
