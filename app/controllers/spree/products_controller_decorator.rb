8e1d314e7603aa063f058e456c7a7504db904532
Ryan Bigg <radarlistener@gmail.com>
2014-02-01 12:33:10 +0400

    Raise an ActiveRecord::RecordNotFound exception in ProductsController
    By rendering the 404 template, it renders within the layout and looks ugly. Raising an AR::RecordNotFound exception here will render the 404 page normally.

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -5,7 +5,7 @@ Spree::ProductsController.class_eval do
   def can_show_product
     @product ||= Spree::Product.find_by_permalink!(params[:id])
     if @product.stores.empty? || !@product.stores.include?(current_store)
-      render :file => "#{::Rails.root}/public/404", :status => 404, :formats => [:html]
+      raise ActiveRecord::RecordNotFound
     end
   end
 
