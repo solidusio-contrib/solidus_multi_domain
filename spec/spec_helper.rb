200d61efb4f77ad31e542a94c2a30928c1631bf5
Ryan Bigg <radarlistener@gmail.com>
2014-02-01 12:55:04 +0400

    Use correct requires for testing support features

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,9 +11,9 @@ require 'rspec/rails'
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f }
 
 # Requires factories defined in spree_core
-require 'spree/core/testing_support/factories'
-require 'spree/core/testing_support/controller_requests'
-require 'spree/core/testing_support/authorization_helpers'
+require 'spree/testing_support/factories'
+require 'spree/testing_support/controller_requests'
+require 'spree/testing_support/authorization_helpers'
 require 'factories'
 
 RSpec.configure do |config|
@@ -35,5 +35,5 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
 
   config.include FactoryGirl::Syntax::Methods
-  config.include Spree::Core::TestingSupport::ControllerRequests
+  config.include Spree::TestingSupport::ControllerRequests
 end
