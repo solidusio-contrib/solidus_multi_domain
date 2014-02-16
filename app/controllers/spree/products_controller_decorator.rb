04e60cb4004cee87bba9327416db4c5b320b05f6
Jeff Dutil <jdutil@burlingtonwebapps.com>
2014-02-16 03:44:29 +0000

    Auto run migrations.

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -2,6 +2,7 @@ Spree::ProductsController.class_eval do
   before_filter :can_show_product, :only => :show
 
   private
+
   def can_show_product
     @product ||= Spree::Product.find_by!(slug: params[:id])
     if @product.stores.empty? || !@product.stores.include?(current_store)
