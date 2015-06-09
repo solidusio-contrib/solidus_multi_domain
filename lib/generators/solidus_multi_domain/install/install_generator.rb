06b207823caf4848d56688c2882635fb731affd3
Adam Mueller <adam@freerunningtech.com>
2015-06-09 16:55:51 -0700

    Fix generator.
    Didn't work after the rename. :(

--- lib/generators/solidus_multi_domain/install/install_generator.rb
+++ lib/generators/solidus_multi_domain/install/install_generator.rb
@@ -1,4 +1,4 @@
-module SpreeMultiDomain
+module SolidusMultiDomain
   module Generators
     class InstallGenerator < Rails::Generators::Base
       class_option :auto_run_migrations, type: :boolean, default: false
