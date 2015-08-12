e8f799678a4e9b76b8d2d847793c4c3326b08914
Magnus von Koeller <hello@magnusvk.com>
2015-08-11 23:13:14 -0400

    Use new extension point for `self.available`
    So that we don't have to copy&paste code that may change from
    `Spree::PaymentMethod`.

--- app/models/spree/payment_method_decorator.rb
+++ app/models/spree/payment_method_decorator.rb
@@ -1,13 +1,9 @@
+Spree::PaymentMethod.available_extra_conditions = -> (payment_method, options) do
+  store = options[:store]
+  store.nil? || store.payment_methods.empty? || store.payment_methods.include?(payment_method)
+end
+
 Spree::PaymentMethod.class_eval do
   has_many :store_payment_methods
   has_many :stores, :through => :store_payment_methods
-
-  def self.available(display_on = 'both', store = nil)
-    result = all.select do |p|
-      p.active &&
-        (p.environment == Rails.env || p.environment.blank?) &&
-        (store.nil? || store.payment_methods.empty? || store.payment_methods.include?(p)) &&
-        (p.display_on == display_on.to_s || p.display_on.blank?)
-    end
-  end
 end
