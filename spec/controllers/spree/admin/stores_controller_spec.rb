e6d524509321acb2b92d1d9bf336df1b834aa0fd
Jordan Brough <git.j@brgh.net>
2015-09-03 08:59:11 -0600

    Add backend/solidus_multi_domain.css
    `*= require spree/backend/solidus_multi_domain` is added to all.css in
    the spec dummy app automatically so we need it to exist if we want to
    be able to actually render any views in our specs.

--- spec/controllers/spree/admin/stores_controller_spec.rb
+++ spec/controllers/spree/admin/stores_controller_spec.rb
@@ -4,6 +4,8 @@ describe Spree::Admin::StoresController do
   stub_authorization!
 
   describe "on :index" do
+    render_views
+
     it "renders index" do
       spree_get :index
       expect(response).to be_success
