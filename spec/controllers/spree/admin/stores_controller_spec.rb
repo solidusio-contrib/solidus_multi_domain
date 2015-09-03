d078609e5cbaee2224855276747ea0537fdb3458
Jordan Brough <git.j@brgh.net>
2015-09-03 09:02:38 -0600

    Remove payment_method.environment from Admin::StoresController#index
    This was removed from Solidus.

--- spec/controllers/spree/admin/stores_controller_spec.rb
+++ spec/controllers/spree/admin/stores_controller_spec.rb
@@ -3,11 +3,22 @@ require 'spec_helper'
 describe Spree::Admin::StoresController do
   stub_authorization!
 
-  describe "on :index" do
+  describe '#index' do
     render_views
 
-    it "renders index" do
+    it 'renders' do
       spree_get :index
+      expect(response).to be_success
+    end
+  end
+
+  describe '#edit' do
+    render_views
+
+    let(:store) { create(:store) }
+
+    it 'renders' do
+      spree_get :edit, id: store.to_param
       expect(response).to be_success
     end
   end
