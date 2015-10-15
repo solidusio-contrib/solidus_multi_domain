2f2f5a21cbc9c14b413e8659e9838efe9df0eb47
John Hawthorn <john@stembolt.com>
2015-10-15 10:16:23 -0700

    Fix for removal of Tracker.environment

--- app/models/spree/tracker_decorator.rb
+++ app/models/spree/tracker_decorator.rb
@@ -2,6 +2,6 @@ Spree::Tracker.class_eval do
   belongs_to :store
 
   def self.current(store_key)
-    Spree::Tracker.where(active: true, environment: Rails.env).joins(:store).where("spree_stores.code = ? OR spree_stores.url LIKE ?", store_key, "%#{store_key}%").first
+    Spree::Tracker.where(active: true).joins(:store).where("spree_stores.code = ? OR spree_stores.url LIKE ?", store_key, "%#{store_key}%").first
   end
 end
