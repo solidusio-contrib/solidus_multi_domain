a57e2f3f63f15e4943899d39530a87fd8b2737d2
Dean Holdren <deanholdren@gmail.com>
2016-04-22 13:13:16 -0400

    use `current_store` when looking up the Spree::Tracker

--- app/models/spree/tracker_decorator.rb
+++ app/models/spree/tracker_decorator.rb
@@ -1,7 +1,7 @@
 Spree::Tracker.class_eval do
   belongs_to :store
 
-  def self.current(store_key)
-    Spree::Tracker.where(active: true).joins(:store).where("spree_stores.code = ? OR spree_stores.url LIKE ?", store_key, "%#{store_key}%").first
+  def self.current(store)
+    Spree::Tracker.where(active: true, store_id: store).first
   end
 end
