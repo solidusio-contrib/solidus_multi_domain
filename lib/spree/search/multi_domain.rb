8d44757146ed976efaf1838c3b45a3e4d1fd90e3
John Hawthorn <john@stembolt.com>
2016-07-05 13:41:55 -0700

    Use .available instead of .active

--- lib/spree/search/multi_domain.rb
+++ lib/spree/search/multi_domain.rb
@@ -1,7 +1,7 @@
 module Spree::Search
   class MultiDomain < Spree::Core::Search::Base
     def get_base_scope
-      base_scope = @cached_product_group ? @cached_product_group.products.active : Spree::Product.active
+      base_scope = @cached_product_group ? @cached_product_group.products.available : Spree::Product.available
       base_scope = base_scope.by_store(current_store_id) if current_store_id
       base_scope = base_scope.in_taxon(taxon) unless taxon.blank?
 
