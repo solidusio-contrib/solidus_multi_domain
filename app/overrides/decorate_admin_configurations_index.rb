e4eb06041aa6c5b7b48b0a11072fd7c852a031e5
Adam Mueller <adam@stembolt.com>
2017-06-26 11:19:46 -0700

    [WIP] Fix admin overrides
    The Solidus admin has changed, but this extension hasn't kept up. Most
    of our view overrides are no longer required or work.

--- app/overrides/decorate_admin_configurations_index.rb
+++ app/overrides/decorate_admin_configurations_index.rb
@@ -1,7 +1,7 @@
-Deface::Override.new(
-  virtual_path: "spree/admin/shared/_configuration_menu",
-  name: "multi_domain_admin_configurations_menu",
-  insert_bottom: "[data-hook='admin_configurations_sidebar_menu']",
-  disabled: false,
-  partial: "spree/shared/multi_domain_sidebar_entry"
-)
+#Deface::Override.new(
+#  virtual_path: "spree/admin/shared/_configuration_menu",
+#  name: "multi_domain_admin_configurations_menu",
+#  insert_bottom: "[data-hook='admin_configurations_sidebar_menu']",
+#  disabled: false,
+#  partial: "spree/shared/multi_domain_sidebar_entry"
+#)
