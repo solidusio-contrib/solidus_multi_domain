71f0aca47a1a560310f9b3a34127c5b871d2930a
Cesar Carruitero <cesar@mozilla.pe>
2017-09-30 12:24:44 -0500

    inject currency field in store form for solidus < 2.4

--- app/overrides/decorate_admin_store_configuration.rb
+++ app/overrides/decorate_admin_store_configuration.rb
@@ -5,3 +5,13 @@ Deface::Override.new(
   partial: 'spree/admin/stores/multi_domain_fields',
   disabled: false
 )
+
+if SolidusSupport.solidus_gem_version < Gem::Version.new('2.4.x')
+  Deface::Override.new(
+    virtual_path: 'spree/admin/stores/_multi_domain_fields',
+    name: 'currency field',
+    insert_top: "[data-hook='admin_store_multi_domain_fields']",
+    partial: 'spree/admin/stores/currency_field',
+    disabled: false
+  )
+end
