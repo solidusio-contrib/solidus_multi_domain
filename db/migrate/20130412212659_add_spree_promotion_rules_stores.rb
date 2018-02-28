5752c7e7f8ea60462a4cc6e1339c8d05e20599b5
Adam Mueller <adam@stembolt.com>
2018-02-28 15:49:11 -0800

    Make migrations safer
    Only create new tables/indexes if they don't already exist.

--- db/migrate/20130412212659_add_spree_promotion_rules_stores.rb
+++ db/migrate/20130412212659_add_spree_promotion_rules_stores.rb
@@ -1,5 +1,7 @@
 class AddSpreePromotionRulesStores < SolidusSupport::Migration[4.2]
   def change
+    return if table_exists?(:spree_promotion_rules_stores)
+
     create_table :spree_promotion_rules_stores, :id => false do |t|
       t.references :promotion_rule
       t.references :store
