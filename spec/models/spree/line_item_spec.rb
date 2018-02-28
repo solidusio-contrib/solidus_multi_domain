d8a75e379f3b5cb4eab520e818b0373eecbadc85
Adam Mueller <adam@stembolt.com>
2018-02-28 15:52:52 -0800

    Remove skipped spec
    This spec has been skipped since Solidus v1.3. No reason to keep it
    around anymore.

--- spec/models/spree/line_item_spec.rb
+++ spec/models/spree/line_item_spec.rb
@@ -11,22 +11,6 @@ describe Spree::LineItem do
 
     subject { line_item.save! }
 
-    context "the order does not have a store" do
-      before do
-        version = Spree.try(:solidus_gem_version)
-        if version && version >= Gem::Version.new("1.3.0.alpha")
-          skip "As of 1.3.0 Orders must have a store"
-        end
-      end
-
-      before(:each) { order.update_attributes(store_id: nil) }
-      let(:product) { create(:product, stores: [order_store])}
-
-      it 'does not raise an error' do
-        expect{ subject }.to_not raise_error
-      end
-    end
-
     context "the line item's product does not belong to the order's store" do
       let(:product) { create(:product, stores: [other_store]) }
 
