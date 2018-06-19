5a04f2c4035080f84d35d438071d846ca98c1a2c
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2018-06-19 11:48:32 -0500

    Fix issue with duplicated migrations

--- db/migrate/20130325231147_add_store_shipping_methods.rb
+++ db/migrate/20130325231147_add_store_shipping_methods.rb
@@ -1,14 +1,16 @@
 class AddStoreShippingMethods < SolidusSupport::Migration[4.2]
   def change
-    create_table :spree_store_shipping_methods do |t|
-      t.integer :store_id
-      t.integer :shipping_method_id
+    unless table_exists?('spree_store_shipping_methods')
+      create_table :spree_store_shipping_methods do |t|
+        t.integer :store_id
+        t.integer :shipping_method_id
 
-      t.timestamps null: true
+        t.timestamps null: true
+      end
+
+      add_index :spree_store_shipping_methods, :store_id
+      add_index :spree_store_shipping_methods, :shipping_method_id
     end
-
-    add_index :spree_store_shipping_methods, :store_id
-    add_index :spree_store_shipping_methods, :shipping_method_id
   end
 end
 
--- db/migrate/20130412212659_add_spree_promotion_rules_stores.rb
+++ db/migrate/20130412212659_add_spree_promotion_rules_stores.rb
@@ -1,8 +1,10 @@
 class AddSpreePromotionRulesStores < SolidusSupport::Migration[4.2]
   def change
-    create_table :spree_promotion_rules_stores, :id => false do |t|
-      t.references :promotion_rule
-      t.references :store
+    unless table_exists?('spree_promotion_rules_stores')
+      create_table :spree_promotion_rules_stores do |t|
+        t.references :store
+        t.references :promotion_rule
+      end
     end
   end
 end
