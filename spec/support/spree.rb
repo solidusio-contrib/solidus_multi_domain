92011c091e6a3079e6cf33e41d1a9d99822e2acd
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-04-05 12:22:30 -0600

    Update structure of test suite

---
+++ spec/support/spree.rb
@@ -0,0 +1,14 @@
+require 'spree/testing_support/factories'
+require 'spree/testing_support/controller_requests'
+require 'spree/testing_support/authorization_helpers'
+require 'spree/testing_support/url_helpers'
+require 'spree/testing_support/preferences'
+require 'spree/api/testing_support/helpers'
+require 'spree/api/testing_support/setup'
+require 'spree/testing_support/capybara_ext'
+
+RSpec.configure do |config|
+  config.include Spree::TestingSupport::ControllerRequests, type: :controller
+  config.include Spree::TestingSupport::UrlHelpers
+  config.include Spree::Api::TestingSupport::Helpers, type: :controller
+end
