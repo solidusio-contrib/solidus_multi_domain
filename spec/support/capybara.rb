ea2becdd8ac9e77e6824877ef9de8ce4d9d5198f
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-04-05 12:22:30 -0600

    Update structure of test suite

---
+++ spec/support/capybara.rb
@@ -0,0 +1,19 @@
+require 'capybara/rspec'
+require 'capybara/rails'
+require 'capybara/poltergeist'
+require 'capybara-screenshot/rspec'
+
+RSpec.configure do |config|
+  Capybara.default_max_wait_time = 10
+  Capybara.javascript_driver = :poltergeist
+
+  Capybara.register_driver(:poltergeist) do |app|
+    Capybara::Poltergeist::Driver.new app, js_errors: true, timeout: 90
+  end
+
+  config.before(:each, :js) do
+    if Capybara.javascript_driver == :selenium
+      page.driver.browser.manage.window.maximize
+    end
+  end
+end
