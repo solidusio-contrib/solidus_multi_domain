bac34b68325d2960e03421b9f92f2f4bc00536c3
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-04-05 12:22:30 -0600

    Update structure of test suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,61 +1,32 @@
-require 'simplecov'
-SimpleCov.start 'rails'
-
 ENV['RAILS_ENV'] ||= 'test'
 
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+begin
+  require File.expand_path('../dummy/config/environment', __FILE__)
+rescue LoadError
+  puts 'Could not load dummy application. Please ensure you have run `bundle exec rake test_app`'
+  exit
+end
 
 require 'rspec/rails'
 require 'ffaker'
-
-require 'database_cleaner'
-require 'capybara/rspec'
-require 'capybara-screenshot/rspec'
-require 'capybara/poltergeist'
-Capybara.register_driver(:poltergeist) do |app|
-  Capybara::Poltergeist::Driver.new app, timeout: 90
-end
-Capybara.javascript_driver = :poltergeist
-Capybara.default_max_wait_time = 10
-
-# Requires factories defined in spree_core
-require 'spree/testing_support/factories'
-require 'spree_multi_domain/testing_support/factory_overrides'
-require 'spree/testing_support/controller_requests'
-require 'spree/testing_support/authorization_helpers'
-require 'spree/testing_support/preferences'
-require 'spree/api/testing_support/helpers'
-require 'spree/api/testing_support/setup'
-require 'spree/testing_support/capybara_ext'
-
+require 'pry'
 require 'cancan/matchers'
 
-Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each { |f| require f }
+Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
-FactoryBot.use_parent_strategy = false
+require 'solidus_multi_domain/factories'
 
 RSpec.configure do |config|
-  config.infer_spec_type_from_file_location!
-  config.mock_with :rspec
-
+  config.fail_fast = false
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
-  config.use_transactional_fixtures = false
 
-  config.include FactoryBot::Syntax::Methods
-  config.include Spree::TestingSupport::ControllerRequests, type: :controller
-  config.include Spree::Api::TestingSupport::Helpers, type: :controller
+  config.mock_with :rspec
+  config.raise_errors_for_deprecations!
+  config.infer_spec_type_from_file_location!
 
-  config.before :suite do
-    DatabaseCleaner.clean_with :truncation
-  end
-
-  config.before do
-    DatabaseCleaner.strategy = RSpec.current_example.metadata[:js] ? :truncation : :transaction
-    DatabaseCleaner.start
-  end
-
-  config.after do
-    DatabaseCleaner.clean
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
   end
 end
+
---
+++ spec/support/factory_bot.rb
@@ -0,0 +1,8 @@
+require 'factory_bot'
+
+FactoryBot.find_definitions
+FactoryBot.use_parent_strategy = false
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
