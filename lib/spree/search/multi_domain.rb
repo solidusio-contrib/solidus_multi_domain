8b98576ab9ac9e8c5e4c73909de06648b629ac2d
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-04-09 13:53:55 -0500

    Add quick fixes for search module

--- lib/spree/search/multi_domain.rb
+++ lib/spree/search/multi_domain.rb
@@ -4,10 +4,9 @@ module Spree::Search
       base_scope = @cached_product_group ? @cached_product_group.products.available : Spree::Product.available
       base_scope = base_scope.by_store(current_store_id) if current_store_id
       base_scope = base_scope.in_taxon(taxon) unless taxon.blank?
-
-      base_scope = get_products_conditions_for(base_scope, keywords) unless keywords.blank?
-
+      base_scope = get_products_conditions_for(base_scope, @properties[:keywords])
       base_scope = add_search_scopes(base_scope)
+      base_scope = add_eagerload_scopes(base_scope)
       base_scope
     end
 
