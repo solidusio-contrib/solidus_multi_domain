50875bbb785b7c5ab1656df625f2f71496089fcc
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-04-10 15:25:13 -0500

    Update products helper

--- app/helpers/spree/products_helper_decorator.rb
+++ app/helpers/spree/products_helper_decorator.rb
@@ -3,7 +3,7 @@ module Spree
     ProductsHelper.class_eval do
       def get_taxonomies
         @taxonomies ||= current_store.present? ? Spree::Taxonomy.where(["store_id = ?", current_store.id]) : Spree::Taxonomy
-        @taxonomies = @taxonomies.includes(:root => :children)
+        @taxonomies = @taxonomies.includes(root: :children)
         @taxonomies
       end
     end
