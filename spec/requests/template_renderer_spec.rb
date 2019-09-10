59be7592eec7791f808bdfdc8b12aef5f48ecf08
Alessandro Desantis <desa.alessandro@gmail.com>
2019-09-10 15:58:31 +0200

    Fix non-deterministic TemplateRenderer spec

--- spec/requests/template_renderer_spec.rb
+++ spec/requests/template_renderer_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe "Template renderer with dynamic layouts" do
-  before(:each) do
+  before do
+    @old_view_paths = ApplicationController.view_paths
+
     ApplicationController.view_paths = [ActionView::FixtureResolver.new(
-        "spree/layouts/spree_application.html.erb"             => "Default layout <%= yield %>",
-        "spree/layouts/my_store/spree_application.html.erb"    => "Store layout <%= yield %>",
-        "application/index.html.erb"                           => "hello"
-      )]
+      "spree/layouts/spree_application.html.erb"             => "Default layout <%= yield %>",
+      "spree/layouts/my_store/spree_application.html.erb"    => "Store layout <%= yield %>",
+      "application/index.html.erb"                           => "hello"
+    )]
+  end
+
+  after do
+    ApplicationController.view_paths = @old_view_paths
   end
 
   let(:store) { FactoryBot.create :store, code: "my_store" }
@@ -22,20 +28,29 @@ describe "Template renderer with dynamic layouts" do
   end
 
   context "for a controller inheriting from ApplicationController" do
-    NormalController = Class.new(ApplicationController)
+    before(:all) do
+      NormalController = Class.new(ApplicationController)
+    end
 
     before do
       ApplicationController.view_paths += [ActionView::FixtureResolver.new(
-          "normal/index.html.erb" => "just normal"
-        )]
+        "normal/index.html.erb" => "just normal"
+      )]
+
       Rails.application.routes.draw do
         get 'normal', to: 'normal#index'
       end
     end
+
+    after do
+      Rails.application.reload_routes!
+    end
+
     it "should fall back to the default layout for unmatched store" do
       get "http://www.example.com/normal"
       expect(response.body).to eq("Default layout just normal")
     end
+
     it "should render the layout for the current store" do
       get "http://#{store.url}/normal"
       expect(response.body).to eq("Store layout just normal")
@@ -43,25 +58,34 @@ describe "Template renderer with dynamic layouts" do
   end
 
   context "with an explicit `layout` passed to render" do
-    ExplicitLayoutController = Class.new(ApplicationController) do
-      def index
-        render :index, layout: 'fancy'
+    before(:all) do
+      ExplicitLayoutController = Class.new(ApplicationController) do
+        def index
+          render :index, layout: 'fancy'
+        end
       end
     end
 
     before do
       ApplicationController.view_paths += [ActionView::FixtureResolver.new(
-          "layouts/fancy.html.erb"         => "Fancy <%= yield %>",
-          "explicit_layout/index.html.erb" => "explicit layout index"
-        )]
+        "layouts/fancy.html.erb"         => "Fancy <%= yield %>",
+        "explicit_layout/index.html.erb" => "explicit layout index"
+      )]
+
       Rails.application.routes.draw do
         get 'explicit_layout', to: 'explicit_layout#index'
       end
     end
+
+    after do
+      Rails.application.reload_routes!
+    end
+
     it "should use explicit layout for unmatched store" do
       get "http://www.example.com/explicit_layout"
       expect(response.body).to eq("Fancy explicit layout index")
     end
+
     it "should use explicit layout for the current store" do
       get "http://#{store.url}/explicit_layout"
       expect(response.body).to eq("Fancy explicit layout index")
