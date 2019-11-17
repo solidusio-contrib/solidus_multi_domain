cf47e6040c5f4bdfdc3626722def88c221b4fddd
stefan <stefan@yo-code.de>
2019-11-17 19:12:33 +0100

    fix for #124

--- app/decorators/controllers/spree_multi_domain/spree/taxons_controller_decorator.rb
+++ app/decorators/controllers/spree_multi_domain/spree/taxons_controller_decorator.rb
@@ -4,7 +4,7 @@ module SpreeMultiDomain
   module Spree
     module TaxonsControllerDecorator
       def show
-        @taxon = Spree::Taxon.find_by!(store_id: current_store.id, permalink: params[:id])
+        @taxon = ::Spree::Taxon.find_by_store_id_and_permalink!(current_store.id, params[:id])
         return unless @taxon
 
         @searcher = build_searcher(params.merge(taxon: @taxon.id))
