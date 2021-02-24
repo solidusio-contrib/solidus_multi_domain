84a3cd96131110390316c9f08042a2cbb2dc60f8
zernie <zernie@users.noreply.github.com>
2021-02-24 16:45:09 +0300

    fix: ShipmentsControllerDecorator namespacing

--- app/decorators/controllers/solidus_multi_domain/spree/api/shipments_controller_decorator.rb
+++ app/decorators/controllers/solidus_multi_domain/spree/api/shipments_controller_decorator.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
-module SpreeMultiStore
-  module Api
-    module ShipmentsControllerDecorator
-      def self.prepended(base)
-        base.prepend SolidusMultiDomain::CreateLineItemSupport
-      end
+module SolidusMultiDomain
+  module Spree
+    module Api
+      module ShipmentsControllerDecorator
+        def self.prepended(base)
+          base.prepend SolidusMultiDomain::CreateLineItemSupport
+        end
 
-      def mine
-        super
-        @shipments = @shipments.where(spree_orders: { store_id: current_store.id }) if @shipments
-      end
+        def mine
+          super
+          @shipments = @shipments.where(spree_orders: { store_id: current_store.id }) if @shipments
+        end
 
-      ::Spree::Api::ShipmentsController.prepend(self) if SolidusMultiDomain::Engine.api_available?
+        ::Spree::Api::ShipmentsController.prepend(self) if SolidusMultiDomain::Engine.api_available?
+      end
     end
   end
 end
