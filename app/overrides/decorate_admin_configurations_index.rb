54e7f292f3d686d4dd0464c0edc3bc07d326a9ec
Harmony Bouvier <harmony@super.gd>
2024-07-25 10:08:56 -0700

    Wrap deface overrides in a module
    Zeitwerk expects a class or module correctly named in these files.
    We can simply create the expected named modules to appease Zeitwerk.

--- app/overrides/decorate_admin_configurations_index.rb
+++ app/overrides/decorate_admin_configurations_index.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-Deface::Override.new(
-  virtual_path: "spree/admin/shared/_configuration_menu",
-  name: "multi_domain_admin_configurations_menu",
-  insert_bottom: "[data-hook='admin_configurations_sidebar_menu']",
-  disabled: false,
-  partial: "spree/shared/multi_domain_sidebar_entry"
-)
+module DecorateAdminConfigurationsIndex
+  Deface::Override.new(
+    virtual_path: "spree/admin/shared/_configuration_menu",
+    name: "multi_domain_admin_configurations_menu",
+    insert_bottom: "[data-hook='admin_configurations_sidebar_menu']",
+    disabled: false,
+    partial: "spree/shared/multi_domain_sidebar_entry"
+  )
+end
--- app/overrides/decorate_admin_products_index.rb
+++ app/overrides/decorate_admin_products_index.rb
@@ -1,25 +1,27 @@
 # frozen_string_literal: true
 
-Deface::Override.new(
-  virtual_path: "spree/admin/products/index",
-  name: "multi_domain_admin_products_index_headers",
-  insert_before: "[data-hook='admin_products_index_header_actions']",
-  partial: "spree/admin/products/index_headers",
-  disabled: false
-)
+module DecorateAdminProductsIndex
+  Deface::Override.new(
+    virtual_path: "spree/admin/products/index",
+    name: "multi_domain_admin_products_index_headers",
+    insert_before: "[data-hook='admin_products_index_header_actions']",
+    partial: "spree/admin/products/index_headers",
+    disabled: false
+  )
 
-Deface::Override.new(
-  virtual_path: "spree/admin/products/index",
-  name: "multi_domain_admin_products_index_rows",
-  insert_before: "[data-hook='admin_products_index_row_actions']",
-  partial: "spree/admin/products/index_rows",
-  disabled: false
-)
+  Deface::Override.new(
+    virtual_path: "spree/admin/products/index",
+    name: "multi_domain_admin_products_index_rows",
+    insert_before: "[data-hook='admin_products_index_row_actions']",
+    partial: "spree/admin/products/index_rows",
+    disabled: false
+  )
 
-Deface::Override.new(
-  virtual_path: "spree/admin/products/index",
-  name: "multi_domain_admin_products_index_search",
-  insert_top: "[data-hook='admin_products_index_search']",
-  partial: "spree/admin/products/index_search_fields",
-  disabled: false
-)
+  Deface::Override.new(
+    virtual_path: "spree/admin/products/index",
+    name: "multi_domain_admin_products_index_search",
+    insert_top: "[data-hook='admin_products_index_search']",
+    partial: "spree/admin/products/index_search_fields",
+    disabled: false
+  )
+end
